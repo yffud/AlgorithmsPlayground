d347fa139c54476614d8a1cb5a53f14bb4dc8c9a
Shane Duffy <sduffy@bb-elec.com>
2015-06-10 15:34:29 -0700

    bubble sort

---
+++ bubbleSort/bubble.py
@@ -0,0 +1,23 @@
+# implementation of bubble sort in Python.
+# note python allows a shortcut without using a temporary variable during the assignments
+
+def bubbleSort(someList):
+	for runNumber in range(len(someList)-1,0,-1):
+		# we are going to step backwards through the list
+		for index in range(runNumber):
+			if someList[index]>someList[index + 1]:
+				tempValue = someList[index]
+				someList[index] = someList[index + 1]
+				someList[index + 1] = tempValue
+
+
+
+
+# setup a list 
+someList = [32,12,54,98,23,10,4,102,64,78]
+
+# run bubble sort
+bubbleSort(someList)
+
+#print the sorted list
+print(someList)
